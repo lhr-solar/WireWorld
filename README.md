4b3f1697ce3fa5de66fc552fb4d422ad582d5cc0
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2023-06-29 18:53:16 -0700

    combiner script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # WireWorld
-This repository contains information the wiring of all the electrical systems in the casr
+This repository contains information the wiring of all the electrical systems in the car.
+
+# Generator script
+
+To use the generator script, simply call `python diagram_generator.py`. This will combine the various xml files representing sub-diagrams into one large XML file which can be opened as a draw.io notebook. 
---
+++ diagram_generator.py
@@ -0,0 +1,28 @@
+import os, re
+
+#first line of concat xml file
+header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+header2 = "<mxfile host=\"app.diagrams.net\">"
+footer = "</mxfile>"
+
+#delete the output file if it already exists
+if os.path.exists("./Master_diagram.xml"):
+    os.remove("./Master_diagram.xml")
+
+#concat xml files
+retstr = ""
+for name in sorted(os.listdir("./diagrams")):
+    #read the file
+    with open("./diagrams/" + name, "r") as f:
+        content = f.read()
+        content = re.sub(".*?xml.*","",content) #strip xml header
+        content = re.sub(".*mxfile.*","",content) #remove mxfile start/end tag
+        retstr += content
+
+# add the header to the top, footer to the bottom
+retstr = header + header2 + "\t" + retstr + footer
+
+
+#write the file
+with open("Master_diagram.xml", "w") as f:
+    f.write(retstr)
